ac4b7e55818dc44220a0054d30502373bc766b15
eltc29 <emile.ltc@gmail.com>
2020-05-25 12:58:33 -0400

    project refactor

--- projects/rqgen/rqgen.js
+++ projects/rqgen/rqgen.js
@@ -1,3 +1,5 @@
+let currentQuote, currentAuthor;
+
 $(document).ready(function() {
     $('.new-quote').click(function() {
         $.ajax({
@@ -5,7 +7,9 @@ $(document).ready(function() {
             type: 'get',
             dataType: 'json',
             async: false,
-            success: function(data){
+            success: function(data) {
+                currentQuote = data.quoteText;
+                currentAuthor = data.quoteAuthor;
                 $('.quote-content').html(`<p>${data.quoteText}</p>`);
                 $('.quote-author').html(`<p>- ${data.quoteAuthor}</p>`);
                 $('.quote-source').html(`Source: <a href='${data.quoteLink}'>link</a>`);
@@ -16,13 +20,13 @@ $(document).ready(function() {
         });
     });
 
-    function twitter(){
-        var windowopen = window.open('https://twitter.com/intent/tweet?hashtags=quotes&related=freecodecamp&text=%22undefined%22%20undefined');
+    $('.twitter').click(function() {
+        var windowopen = window.open(`https://twitter.com/intent/tweet?hashtags=quotes&related=freecodecamp&text="${currentQuote}"%20by%20${currentAuthor}`);
         $.html(windowopen);
-    }
+    });
 
-    function tumblr(){
-        var windowopen = window.open('https://www.tumblr.com/login?redirect_to=https%3A%2F%2Fwww.tumblr.com%2Fwidgets%2Fshare%2Ftool%3Fposttype%3Dquote%26tags%3Dquotes%252Cfreecodecamp%26caption%3Dundefined%26content%3Dundefined%26canonicalUrl%3Dhttps%253A%252F%252Fwww.tumblr.com%252Fbuttons%26shareSource%3Dtumblr_share_button" class="fa fa-tumblr');
+    $('.tumblr').click(function() {
+        var windowopen = window.open(`https://www.tumblr.com/login?redirect_to=https%3A%2F%2Fwww.tumblr.com%2Fwidgets%2Fshare%2Ftool%3Fposttype%3Dquote%26tags%3Dquotes%252Cfreecodecamp%26caption%3Dundefined%26content%3Dundefined%26canonicalUrl%3Dhttps%253A%252F%252Fwww.tumblr.com%252Fbuttons%26shareSource%3Dtumblr_share_button`);
         $.html(windowopen);
-    }
+    });
 });
